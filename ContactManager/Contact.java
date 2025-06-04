5d4d8484c812e929c184d70c6f75c2997da24ab8
cverhulle <corentin.verhulle@gmail.com>
2025-06-04 11:41:19 +0200

    Création de la classe Contact

---
+++ ContactManager/Contact.java
@@ -0,0 +1,11 @@
+package ContactManager;
+
+// Cette classe a pour but de créer l'objet "Contact". 
+public class Contact {
+
+    // On fournit quatre propriétés à cette classe.
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+}
