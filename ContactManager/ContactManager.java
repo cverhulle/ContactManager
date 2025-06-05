a0fce7ea3eacd481327b5640816045a34f20eca3
cverhulle <corentin.verhulle@gmail.com>
2025-06-05 10:50:18 +0200

    Ajout de la méthode addContact

--- ContactManager/ContactManager.java
+++ ContactManager/ContactManager.java
@@ -7,10 +7,20 @@ import java.util.List;
 public class ContactManager {
     
     // On crée une variable pour stocker la liste des contacts.
-    private List<Contact> contacts;
+    final private List<Contact> contacts;
 
     // Mise en place du constructeur de la classe.
     public ContactManager() {
-        this.contacts = new ArrayList<Contact>();
+        this.contacts = new ArrayList<>();
+    }
+
+    // Ajout d'une méthode pour ajouter un contact.
+    public void addContact(Contact contact) {
+        if (contacts.contains(contact)) {
+            System.out.println("⚠️ Ce contact existe déjà (même email).");
+        } else {
+            contacts.add(contact);
+            System.out.println("✅ Contact ajouté : " + contact.getFirstName() + " " + contact.getLastName());
+        }
     }
 }
