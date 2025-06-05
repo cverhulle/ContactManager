217dfd5e354cb2c48350f277c9bc368eb9f637c0
cverhulle <corentin.verhulle@gmail.com>
2025-06-05 10:46:29 +0200

    Ajout de commentaires

--- ContactManager/ContactManager.java
+++ ContactManager/ContactManager.java
@@ -3,6 +3,7 @@ package contactmanager;
 import java.util.ArrayList;
 import java.util.List;
 
+// Cette classe permet de gérer le carnet d'adresse.
 public class ContactManager {
     
     // On crée une variable pour stocker la liste des contacts.
