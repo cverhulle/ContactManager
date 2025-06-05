03e6a325da4b8f5508ca36da19b895806745b976
cverhulle <corentin.verhulle@gmail.com>
2025-06-05 11:16:15 +0200

    On affiche la liste des contacts dans main

--- ContactManager/Main.java
+++ ContactManager/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Contact contact = new Contact("Jean", "Dupont", "jean.dupont@gmail.com", "0321498623");
-        System.out.println(contact);
 
         ContactManager contacts = new ContactManager();
 
         Contact contact2 = new Contact("Jean", "Dupond", "jean.dupond@gmail.com", "0321126873");
         contacts.addContact(contact2);
+
+        System.out.println(contacts);
     }
 }
