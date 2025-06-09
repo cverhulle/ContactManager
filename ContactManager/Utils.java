ef36aa580dcca36863734e4f893fe656a8df0e21
cverhulle <corentin.verhulle@gmail.com>
2025-06-09 15:31:14 +0200

    Implémentation de normalizePhone dans Utils

--- ContactManager/Utils.java
+++ ContactManager/Utils.java
@@ -2,4 +2,8 @@ package contactmanager;
 
 public class Utils {
 
+    // Cette méthode permet de supprimer les espaces dans un numéro de téléphone.
+    public static String normalizePhone(String phone) {
+        return phone.replaceAll("\\s+", "");
+    }
 }
