c6831483c95e9eda4edd9f003b0a4e06ad7b88ef
cverhulle <corentin.verhulle@gmail.com>
2025-06-09 14:51:45 +0200

    Test de addContact dans le main

--- ContactManager/Main.java
+++ ContactManager/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Contact contact1 = new Contact("Jean", "Dupont", "jean.dupont@gmail.com", "0321498623");
-        Contact contact2 = new Contact("Jean", "Dupont", "jean.dupont@gmail.com", "0321498623");
+        Contact contact2 = new Contact("Jean", "Dupont", "jeanne.dupont@gmail.com", "03 21 49 86 23");
 
         ContactManager contacts = new ContactManager();
 
