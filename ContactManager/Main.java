4d25be45f7b9bdc8bead2f315908708bca358a3f
cverhulle <corentin.verhulle@gmail.com>
2025-06-09 15:49:23 +0200

    On teste les fonctionnalités dans le main.

--- ContactManager/Main.java
+++ ContactManager/Main.java
@@ -9,10 +9,17 @@ public class Main {
 
         ContactManager contacts = new ContactManager();
 
-        contacts.addContact(contact1);
-        contacts.addContact(contact2);
-        contacts.addContact(contact3);
+        contacts.addContact(contact1); // Le contact est ajouté
+        contacts.addContact(contact2); // Idem
+        contacts.addContact(contact3); // Le contact n'est pas ajouté (numéro de téléphone)
+        
+        System.out.println(contacts);
+
+        contacts.removeContactById(1); // Supprime l'id numéro 1
+        contacts.removeContactById(3); // Ne fait rien
 
         System.out.println(contacts);
+
+
     }
 }
