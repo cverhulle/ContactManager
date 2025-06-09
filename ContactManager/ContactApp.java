b9d965e55dc2beac1aab771269f3349315730abe
cverhulle <corentin.verhulle@gmail.com>
2025-06-09 16:09:14 +0200

    Description de la classe. Ajout des premières variables

--- ContactManager/ContactApp.java
+++ ContactManager/ContactApp.java
@@ -1,5 +1,11 @@
 package contactmanager;
 
-public class ContactApp {
+// Cette classe permet de gérer le déroulé de l'application en fonction des réponses de l'utilisateur
 
+import java.util.Scanner;
+
+public class ContactApp {
+    private ContactManager contacts;
+    private Scanner scanner;
+    
 }
