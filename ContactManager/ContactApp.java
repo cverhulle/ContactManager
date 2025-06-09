54a2c03b36e752ee501c1e4dd48da096f0868bed
cverhulle <corentin.verhulle@gmail.com>
2025-06-09 16:12:44 +0200

    On débute l'implémentation de run()

--- ContactManager/ContactApp.java
+++ ContactManager/ContactApp.java
@@ -14,4 +14,22 @@ public class ContactApp {
         this.contacts = new ContactManager();
         this.scanner = new Scanner(System.in);
     }
+
+    // Cette méthode est utilisée lorsque le programme est en cours
+    public void run() {
+        boolean running = true;
+
+        // Tant que le programme est en cours, on affiche le menu.
+        while(running) {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Ajouter un contact");
+            System.out.println("2. Supprimer un contact");
+            System.out.println("3. Afficher les contacts");
+            System.out.println("4. Quitter");
+            System.out.print("Choix : ");
+
+            // On récupère la réponse de l'utilisateur
+            String choice = scanner.nextLine();
+        }
+    }
 }
