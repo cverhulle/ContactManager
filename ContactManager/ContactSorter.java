e70516de6be3efeef2af6aa48ad1a3d491b3a019
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 15:21:50 +0200

    Création de la classe qui gérera le tri.

---
+++ ContactManager/ContactSorter.java
@@ -0,0 +1,54 @@
+package contactmanager;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ContactSorter {
+
+    @SuppressWarnings("FieldMayBeFinal")
+    private Scanner scanner;
+
+    // On crée le constructeur de la classe : 
+    // Il initialise le scanner.
+    public ContactSorter(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    // Cette méthode permet de gérer le tri d'une liste de contact.
+    public List<Contact> handleSortingContact(List<Contact> inputList) {
+        // On initialise les variables pour le tri
+        String[] options = { "Prénom", "Nom", "Email", "Téléphone", "Tag"};
+
+        // On demande à l'utilisateur s'il veut trier ou revenir en arrière
+        System.out.println("\nVoulez-vous trier les résultats ?");
+
+        try{
+            // On demande sur quel attribut s'effectuera le tri
+            String choice = InputUtils.askMenuChoice(scanner, "Choisissez le filtre pour trier les données", options, "Quitter", false);
+
+            // On demande si le tri est croissant ou décroissant
+            boolean ascending = InputUtils.askSortDirectionInBoolean(scanner);
+
+            // En fonction des choix réalisés, on appelle la méthode correspondante.
+            return getSortedContactsByChoice(inputList, choice, ascending);
+        }  
+        // Si l'utlisateur déclenche l'erreur (en tapant 0), on annule la recherche et, on affiche un message.
+        catch (UserCancelledException e) {
+            System.out.println("Retour au menu principal.");
+            return inputList;
+        }
+    }
+
+    // Cette méthode retourne la liste de contacts entières triés selon le choix et dans l'ordre donné par ascending.
+    private List<Contact> getSortedContactsByChoice(List<Contact> inputList, String choice, boolean ascending) {
+        return switch (choice) {
+            case "1" -> SortUtils.getContactsSortedByFirstName(inputList, ascending);
+            case "2" -> SortUtils.getContactsSortedByLastName(inputList, ascending);
+            case "3" -> SortUtils.getContactsSortedByEmail(inputList, ascending);
+            case "4" -> SortUtils.getContactsSortedByPhoneNumber(inputList, ascending);
+            case "5" -> SortUtils.getContactsSortedByTag(inputList, ascending);
+            default -> inputList;
+        };
+    }
+
+}
