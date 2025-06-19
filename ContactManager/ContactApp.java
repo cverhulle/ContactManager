17d2ab80a5a72653c0b48e4d7aa823919565febd
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 17:15:55 +0200

    On renomme handledisplaycontact en displayContact.

--- ContactManager/ContactApp.java
+++ ContactManager/ContactApp.java
@@ -69,7 +69,7 @@ public class ContactApp {
         switch (choice) {
             case "1" -> adder.addContact();
             case "2" -> remover.removeContact();
-            case "3" -> handleDisplayContacts();
+            case "3" -> displayContacts();
             case "4" -> findContact();
             case "5" -> modifyContact();
             case "6" -> {
@@ -83,7 +83,7 @@ public class ContactApp {
     }
 
     // Cette méthode permet de gérer le choix "Afficher les contacts"
-    private void handleDisplayContacts(){
+    private void displayContacts(){
 
         // Tout d'abord, on affiche tous les contacts.
         displayer.displayContacts(contacts.getAllContacts());
