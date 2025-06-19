baac5bcd4e8a3aa9e3db7fec6553796dc561a4e1
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 17:18:44 +0200

    Implémentation de modifyContact.

--- ContactManager/ContactApp.java
+++ ContactManager/ContactApp.java
@@ -112,41 +112,7 @@ public class ContactApp {
     // Cette méthode permet de modifier un Contact.
     private void modifyContact() {
 
-        // On récupère l'id du contact à modifier.
-        Integer id = askContactId("de modification");
-
-        if (id == null) return;
-
-        // On récupère les données du contact à modifier
-        Contact contact = contacts.getContactById(id);
-        if (contact == null) {
-            System.out.println("Aucun contact trouvé avec cet ID.");
-            return;
-        }
         // On lance la méthode pour lancer la modification.
-        handleModificationContact(id, contact);
-    }
-
-    // Cette méthode permet de gérer la modification de contact.
-    private void handleModificationContact(Integer id, Contact contact) {
-
-        try{
-            // On entre les données à modifier
-            System.out.println("Laissez vide pour ne pas modifier un champ.");
-
-            // On crée un contact mis à jour à partir des données du contact précédent.
-            Contact updated = buildContactFromUserInput(contact);
-
-            // On met à jour le contact avec les données.
-            boolean success = contacts.updateContact(id, updated.getFirstName(), updated.getLastName(), 
-                                                        updated.getEmail(), updated.getPhoneNumber(), updated.getTag()); 
-
-            // Si les données sont "valables", on affiche un message de succès. Sinon, c'est un message d'erreur.
-            System.out.println(success ? "Contact modifié avec succès !" : "Erreur lors de la modification.");
-
-        } // Si l'utlisateur déclenche l'erreur (en tapant 0), on annule la modification et, on affiche un message.
-        catch (UserCancelledException e) {
-            System.out.println("Modification annulée");
-        }
+        modifier.modifyContact();
     }
 }
