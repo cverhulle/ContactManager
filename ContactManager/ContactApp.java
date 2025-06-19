6ea553c100dbd481a3a048c08e51c232f5718493
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 17:16:50 +0200

    On supprime les méthodes inutiles.

--- ContactManager/ContactApp.java
+++ ContactManager/ContactApp.java
@@ -127,52 +127,6 @@ public class ContactApp {
         handleModificationContact(id, contact);
     }
 
-    // Cette méthode permet de centraliser la demande d'un id de contact.
-    private Integer askContactId(String action) {
-        // On demande l'id de contact.
-        try {
-            return InputUtils.askIdContact(scanner);
-
-        // Si l'utilisateur choisit d'annuler, on gère l'erreur
-        } catch (UserCancelledException e) {
-            System.out.println("Action " + action + " annulée.");
-            return null;
-        }
-    }
-
-    // Cette méthode permet de créer un contact à partir des entrées utilisateur.
-    private Contact buildContactFromUserInput(Contact existingContact) throws UserCancelledException {
-        String firstName = InputUtils.askField(scanner, "Prénom", 
-                                        existingContact == null ? null : existingContact.getFirstName(), 
-                                        existingContact != null);
-        String lastName = InputUtils.askField(scanner, "Nom", 
-                                        existingContact == null ? null : existingContact.getLastName(), 
-                                        existingContact != null);
-        String email = InputUtils.askField(scanner, "Email", 
-                                        existingContact == null ? null : existingContact.getEmail(), 
-                                        existingContact != null);
-        String phone = InputUtils.askField(scanner, "Téléphone", 
-                                        existingContact == null ? null : existingContact.getPhoneNumber(), 
-                                        existingContact != null);
-        Tags tag = InputUtils.askTag(scanner, "Tag", 
-                                        existingContact == null ? null : existingContact.getTag(), 
-                                        existingContact != null);
-
-        return new Contact(lastName, firstName, email, phone, tag);
-    }
-
-    // Cette méthode retourne la liste de contacts entières triés selon le choix et dans l'ordre donné par ascending.
-    private List<Contact> getSortedContactsByChoice(List<Contact> inputList, String choice, boolean ascending) {
-        return switch (choice) {
-            case "1" -> SortUtils.getContactsSortedByFirstName(inputList, ascending);
-            case "2" -> SortUtils.getContactsSortedByLastName(inputList, ascending);
-            case "3" -> SortUtils.getContactsSortedByEmail(inputList, ascending);
-            case "4" -> SortUtils.getContactsSortedByPhoneNumber(inputList, ascending);
-            case "5" -> SortUtils.getContactsSortedByTag(inputList, ascending);
-            default -> inputList;
-        };
-    }
-
     // Cette méthode permet de gérer la modification de contact.
     private void handleModificationContact(Integer id, Contact contact) {
 
@@ -193,31 +147,6 @@ public class ContactApp {
         } // Si l'utlisateur déclenche l'erreur (en tapant 0), on annule la modification et, on affiche un message.
         catch (UserCancelledException e) {
             System.out.println("Modification annulée");
-        }
-    }
-
-    // Cette méthode permet de gérer le tri d'une liste de contact.
-    private List<Contact> handleSortingContact(List<Contact> inputList) {
-        // On initialise les variables pour le tri
-        String[] options = { "Prénom", "Nom", "Email", "Téléphone", "Tag"};
-
-        // On demande à l'utilisateur s'il veut trier ou revenir en arrière
-        System.out.println("\nVoulez-vous trier les résultats ?");
-
-        try{
-            // On demande sur quel attribut s'effectuera le tri
-            String choice = InputUtils.askMenuChoice(scanner, "Choisissez le filtre pour trier les données", options, "Quitter", false);
-
-            // On demande si le tri est croissant ou décroissant
-            boolean ascending = InputUtils.askSortDirectionInBoolean(scanner);
-
-            // En fonction des choix réalisés, on appelle la méthode correspondante.
-            return getSortedContactsByChoice(inputList, choice, ascending);
-        }  
-        // Si l'utlisateur déclenche l'erreur (en tapant 0), on annule la recherche et, on affiche un message.
-        catch (UserCancelledException e) {
-            System.out.println("Retour au menu principal.");
-            return inputList;
         }
     }
 }
