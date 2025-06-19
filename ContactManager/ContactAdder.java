d54ef6effb99564c5aff19569b8f2c6db851245b
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 15:30:34 +0200

    Utilisation de ContactInputHelpers dans addContact.

--- ContactManager/ContactAdder.java
+++ ContactManager/ContactAdder.java
@@ -22,33 +22,12 @@ public class ContactAdder {
 
         // On demande les champs du contact à l'utilisateur
         try{ 
-            Contact newContact = buildContactFromUserInput(null);
+            Contact newContact = ContactInputHelpers.buildContactFromUserInput(scanner, null);
             contacts.addContact(newContact);
         
         // Si l'utilisateur tape 0, on récupère l'erreur et on annule l'ajout.
         } catch (UserCancelledException e) {
             System.out.println("Ajout annulé.");
         }
-    }
-
-    // Cette méthode permet de créer un contact à partir des entrées utilisateur.
-    private Contact buildContactFromUserInput(Contact existingContact) throws UserCancelledException {
-        String firstName = InputUtils.askField(scanner, "Prénom", 
-                                        existingContact == null ? null : existingContact.getFirstName(), 
-                                        existingContact != null);
-        String lastName = InputUtils.askField(scanner, "Nom", 
-                                        existingContact == null ? null : existingContact.getLastName(), 
-                                        existingContact != null);
-        String email = InputUtils.askField(scanner, "Email", 
-                                        existingContact == null ? null : existingContact.getEmail(), 
-                                        existingContact != null);
-        String phone = InputUtils.askField(scanner, "Téléphone", 
-                                        existingContact == null ? null : existingContact.getPhoneNumber(), 
-                                        existingContact != null);
-        Tags tag = InputUtils.askTag(scanner, "Tag", 
-                                        existingContact == null ? null : existingContact.getTag(), 
-                                        existingContact != null);
-
-        return new Contact(lastName, firstName, email, phone, tag);
     }
 }
