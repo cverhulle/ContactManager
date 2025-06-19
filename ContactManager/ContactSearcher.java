a4de1cdf73c1039832db3f386c5532870eb04aa1
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 15:45:09 +0200

    Création du fichier pour gérer la recherche de contact.

--- ContactManager/ContactSearcher.java
+++ ContactManager/ContactSearcher.java
@@ -1,5 +1,6 @@
 package contactmanager;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ContactSearcher {
@@ -14,6 +15,57 @@ public class ContactSearcher {
     public ContactSearcher(ContactManager contacts, Scanner scanner) {
         this.contacts = contacts;
         this.scanner = scanner;
+    }
+
+    // Cette méthode permet de rechercher un contact lorsqu'on est dans le cas Tag.
+    // Elle retourne la liste de contacts correspondant au critère.
+    private List<Contact> findContactByTag() {
+
+        // On affiche toutes les options de tag
+        Tags tagChoice = InputUtils.askTag(scanner, "Choisissez un tag :", null, true);
+
+        // Si l'entrée est vide, on affiche tous les contacts.
+        if (tagChoice == null) {
+           return contacts.getAllContacts();
+        } else {
+            // Sinon, on affiche les résultats.
+            return SearchUtils.searchByTag(contacts.getAllContacts(), tagChoice);
+        }
+    }
+
+    // Cette méthode permet de rechercher un contact
+    public List<Contact> handleSortingContact() {
+
+        // On affiche la liste des champs de recherche possible.
+        String[] options = { "Prénom", "Nom", "Email", "Téléphone", "Tag" };
+
+        try{ 
+            String fieldChoice = InputUtils.askMenuChoice(scanner, "Rechercher par :", options, "Annuler la recherche",false);
+
+            // Si on choisit de chercher par Tags.
+            if ("5".equals(fieldChoice)) { 
+                return findContactByTag();
+            
+            // Si l'option Tag n'est pas chosie
+            } else {
+
+                // On demande la donnée à rechercher
+                String query = InputUtils.askInput(scanner, "Entrez la valeur à rechercher : ");
+
+                // On lance la recherche correspondante
+                return switch (fieldChoice) {
+                    case "1" -> SearchUtils.searchByFirstName(contacts.getAllContacts(), query);
+                    case "2" -> SearchUtils.searchByLastName(contacts.getAllContacts(), query);
+                    case "3" -> SearchUtils.searchByEmail(contacts.getAllContacts(), query);
+                    case "4" -> SearchUtils.searchByPhone(contacts.getAllContacts(), query);
+                    default -> throw new IllegalStateException("Unexpected value: " + fieldChoice);
+                };
+            }  
+        } // Si l'utlisateur déclenche l'erreur (en tapant 0), on annule la recherche et, on affiche un message.
+        catch (UserCancelledException e) {
+            System.out.println("Recherche annulée");
+            return null;
+        }
     }
 
 }
