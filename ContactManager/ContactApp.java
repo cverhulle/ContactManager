d8c70d1c7e11ac9491597c5f6d3da8907533c86f
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 11:26:30 +0200

    Implémentation de handleFindingContact()

--- ContactManager/ContactApp.java
+++ ContactManager/ContactApp.java
@@ -283,6 +283,41 @@ public class ContactApp {
         };
     }
 
+    // Cette méthode permet de gérer la recherche de contact
+    private List<Contact> handleFindingContact() {
+
+        // On affiche la liste des champs de recherche possible.
+        String[] options = { "Prénom", "Nom", "Email", "Téléphone", "Tag" };
+
+        try{ 
+            String fieldChoice = Utils.askMenuChoice(scanner, "Rechercher par :", options, "Annuler la recherche",false);
+
+            // Si on choisit de chercher par Tags.
+            if ("5".equals(fieldChoice)) { 
+                return findContactByTag();
+            
+            // Si l'option Tag n'est pas chosie
+            } else {
+
+                // On demande la donnée à rechercher
+                String query = Utils.askInput(scanner, "Entrez la valeur à rechercher : ");
+
+                // On lance la recherche correspondante
+                return switch (fieldChoice) {
+                    case "1" -> contacts.searchByFirstName(query);
+                    case "2" -> contacts.searchByLastName(query);
+                    case "3" -> contacts.searchByEmail(query);
+                    case "4" -> contacts.searchByPhone(query);
+                    default -> throw new IllegalStateException("Unexpected value: " + fieldChoice);
+                };
+            }  
+        } // Si l'utlisateur déclenche l'erreur (en tapant 0), on annule la recherche et, on affiche un message.
+        catch (UserCancelledException e) {
+            System.out.println("Recherche annulée");
+            return null;
+        }
+    }
+
     // Cette méthode permet de gérer le tri d'une liste de contact.
     private List<Contact> handleSortingContact(List<Contact> inputList) {
         // On initialise les variables pour le tri
