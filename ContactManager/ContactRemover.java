25a0749b9ead7a99cc15b8e90db8bbd0559cb836
cverhulle <corentin.verhulle@gmail.com>
2025-06-19 15:11:30 +0200

    On récupère les fonctions liées à la suppression de contact.

--- ContactManager/ContactRemover.java
+++ ContactManager/ContactRemover.java
@@ -16,4 +16,57 @@ public class ContactRemover {
         this.scanner = scanner;
     }
 
+    // Cette méthode permet de supprimer un contact.
+    private void removeContact () {
+
+        // On demande à l'utilisateur l'id à rechercher.
+        Integer id = askContactId("de suppression");
+
+        // On gère les cas d'erreur
+        if (id == null) return;
+
+        // Si tout est ok, on appelle la méthode suivante.
+        confirmRemoveContact(id);
+    }
+
+    // Cette méthode permet de confirmer la suppression d'un contact
+    private void confirmRemoveContact(Integer id) {
+
+        // On récupère le contact grâce à son id.
+        Contact contact = contacts.getContactById(id);
+
+        // S'il est trouvé...
+        if (contact != null) {
+
+            // On demande à l'utilisateur de confirmer la suppression
+            String message = "Voulez-vous vraiment supprimer ce contact ? " + contact;
+
+            // Si c'est confirmé...
+            if (InputUtils.confirmChoice(scanner, message)) {
+
+                // On supprime le contact.
+                contacts.removeContactById(id);
+
+            // Sinon, on annule la suppression.
+            } else {
+                System.out.println("Suppression annulée.");
+            }
+        } else {
+            System.out.println("Aucun contact trouvé avec cet ID.");
+        }
+    }
+
+    // Cette méthode permet de centraliser la demande d'un id de contact.
+    private Integer askContactId(String action) {
+        // On demande l'id de contact.
+        try {
+            return InputUtils.askIdContact(scanner);
+
+        // Si l'utilisateur choisit d'annuler, on gère l'erreur
+        } catch (UserCancelledException e) {
+            System.out.println("Action " + action + " annulée.");
+            return null;
+        }
+    }
+
 }
