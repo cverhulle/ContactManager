ca322f7789133027480d65d92ffbe46a52c67710
cverhulle <corentin.verhulle@gmail.com>
2025-06-20 11:12:42 +0200

    Implémentation de importContacts

--- ContactManager/features/ContactImporterExporter.java
+++ ContactManager/features/ContactImporterExporter.java
@@ -1,6 +1,7 @@
 package contactmanager.features;
 
 import contactmanager.core.Contact;
+import contactmanager.core.ContactManager;
 import contactmanager.io.ContactIO;
 import java.util.List;
 
@@ -9,5 +10,24 @@ public class ContactImporterExporter {
     // Cette méthode permet de gérer l'export de contact.
     public static void exportContacts(List<Contact> contacts) {
         ContactIO.exportToCSV(contacts);
+    }
+
+    // Cette méthode permet de gérer l'import de Contact.
+    public static void importContacts(ContactManager contacts) {
+
+        // On récupère la liste des contacts avec la méthode d'import.
+        List<Contact> imported = ContactIO.importFromCSV();
+        int added = 0;
+
+        // On crée une boucle pour ne pas ajouter les contacts déjà dans notre liste.
+        for (Contact contact : imported) {
+            if (!contacts.getAllContacts().stream().anyMatch(
+                    c -> c.getEmail().equalsIgnoreCase(contact.getEmail())
+                        || c.getPhoneNumber().equals(contact.getPhoneNumber()))) {
+                contacts.addContact(contact);
+                added++;
+            }
+        }
+        System.out.println("Import terminé : " + added + " contacts ajoutés.");
     }
 }
