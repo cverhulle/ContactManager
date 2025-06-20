9de90d82ff261c1f30b1903c4d6e7c96fd8978c6
cverhulle <corentin.verhulle@gmail.com>
2025-06-20 11:56:41 +0200

    On adapte exportContacts

--- ContactManager/features/ContactImporterExporter.java
+++ ContactManager/features/ContactImporterExporter.java
@@ -3,17 +3,20 @@ package contactmanager.features;
 import contactmanager.core.Contact;
 import contactmanager.core.ContactManager;
 import contactmanager.io.ContactIO;
+import contactmanager.utils.InputUtils;
 import java.util.List;
+import java.util.Scanner;
 
 public class ContactImporterExporter {
 
     // Cette méthode permet de gérer l'export de contact.
-    public static void exportContacts(List<Contact> contactsToExport) {
-        ContactIO.exportToCSV(contactsToExport);
+    public static void exportContacts(Scanner scanner, List<Contact> contactsToExport) {
+        String filePath = InputUtils.askInput(scanner, "Chemin du fichier pour exporter (ex: contacts.csv) : ");
+        ContactIO.exportToCSV(contactsToExport, filePath);
     }
 
     // Cette méthode permet de gérer l'import de Contact.
-    public static void importContacts(ContactManager contacts) {
+    public static void importContacts(Scanner scanner, ContactManager contacts) {
 
         // On récupère la liste des contacts avec la méthode d'import.
         List<Contact> imported = ContactIO.importFromCSV();
