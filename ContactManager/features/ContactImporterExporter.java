bab27c38ad4b765e641ed9deb216856c2c832688
cverhulle <corentin.verhulle@gmail.com>
2025-06-23 17:02:09 +0200

    On range les méthodes par ordre alphabétique.

--- ContactManager/features/ContactImporterExporter.java
+++ ContactManager/features/ContactImporterExporter.java
@@ -9,6 +9,22 @@ import java.util.Scanner;
 
 public class ContactImporterExporter {
 
+    // Cette méthode permet de charger automatiquement les contacts au démarrage de l'application
+    public static void autoLoadContacts(ContactManager contacts) {
+
+        // On appelle la méthode pour gérer l'import
+        Integer added = handleContactImportationFromAPath(contacts, ContactIO.getAutoSavePath());
+
+        // On affiche un message pour l'utilisateur
+        System.out.println("Chargement automatique : " + added + " contacts restaurés.");
+    }
+
+    // Cette méthode permet de sauvegarder automatiquement les contacts.
+    public static void autoSaveContacts(List<Contact> contactsToExport) {
+        ContactIO.exportToCSV(contactsToExport, ContactIO.getAutoSavePath());
+        System.out.println("Sauvegarde automatique effectuée.");
+    }
+
     // Cette méthode permet de gérer l'export de contact.
     public static void exportContacts(Scanner scanner, List<Contact> contactsToExport) {
 
@@ -20,35 +36,6 @@ public class ContactImporterExporter {
 
         // Message d'information à la fin de l'écriture du fichier.
         System.out.println("Contacts exportés avec succès !");
-    }
-
-    // Cette méthode permet de gérer l'import de Contact.
-    public static void importContacts(Scanner scanner, ContactManager contacts) {
-
-        // On demande le chemin du fichier à importer.
-        String filePath = InputUtils.askInput(scanner, "Chemin du fichier pour exporter (ex: contacts.csv) : ");
-        
-        // On appelle la méthode pour gérer l'import
-        Integer added = handleContactImportationFromAPath(contacts, filePath);
-
-            // On affiche un message pour l'utilisateur
-        System.out.println("Import terminé : " + added + " contacts ajoutés.");
-    }
-
-    // Cette méthode permet de sauvegarder automatiquement les contacts.
-    public static void autoSaveContacts(List<Contact> contactsToExport) {
-        ContactIO.exportToCSV(contactsToExport, ContactIO.getAutoSavePath());
-        System.out.println("Sauvegarde automatique effectuée.");
-    }
-
-    // Cette méthode permet de charger automatiquement les contacts au démarrage de l'application
-    public static void autoLoadContacts(ContactManager contacts) {
-
-        // On appelle la méthode pour gérer l'import
-        Integer added = handleContactImportationFromAPath(contacts, ContactIO.getAutoSavePath());
-
-        // On affiche un message pour l'utilisateur
-        System.out.println("Chargement automatique : " + added + " contacts restaurés.");
     }
 
     // Cette méthode s'occupe de la gestion de l'import de contact.
@@ -70,7 +57,19 @@ public class ContactImporterExporter {
                 added++;
             }
         }
-
         return added;
+    }
+
+    // Cette méthode permet de gérer l'import de Contact.
+    public static void importContacts(Scanner scanner, ContactManager contacts) {
+
+        // On demande le chemin du fichier à importer.
+        String filePath = InputUtils.askInput(scanner, "Chemin du fichier pour exporter (ex: contacts.csv) : ");
+
+        // On appelle la méthode pour gérer l'import
+        Integer added = handleContactImportationFromAPath(contacts, filePath);
+
+            // On affiche un message pour l'utilisateur
+        System.out.println("Import terminé : " + added + " contacts ajoutés.");
     }
 }
