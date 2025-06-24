6ca094991db9fa20574dcaf8b784e4a9e2b52538
cverhulle <corentin.verhulle@gmail.com>
2025-06-24 12:00:27 +0200

    Ajout de commentaires

--- ContactManager/Main.java
+++ ContactManager/Main.java
@@ -2,6 +2,8 @@ package contactmanager;
 
 import contactmanager.app.ContactApp;
 
+// Fichier principal de l'application.
+// S'occupe de lancer le programme.
 public class Main {
 
     public static void main(String[] args) {
