78603d6f1e7463bd4c167cdbd7672e7f899b8d9e
cverhulle <corentin.verhulle@gmail.com>
2025-06-24 12:02:34 +0200

    Ajout d'un descriptif de la classe.

--- ContactManager/exception/UserCancelledException.java
+++ ContactManager/exception/UserCancelledException.java
@@ -1,5 +1,7 @@
 package contactmanager.exception;
 
+// Cette classe a pour but de gérer l'entrée "0" du programme.
+// Celle-ci permet d'annuler la saisie et de retourner au menu principal.
 public class UserCancelledException extends RuntimeException {
     public UserCancelledException() {
         super("Entrée annulée par l'utilisateur.");
